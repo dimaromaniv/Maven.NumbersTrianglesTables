dcf52962298ad0f89a7e9c482b6d5e1ecc769991
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-07-04 16:23:51 -0400

    created a multiplication table wihh help of google

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,29 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+
+        StringBuilder result  = new StringBuilder();
+
+        for(int row = 1 ; row <= tableSize ; row++) {
+            for (int col = 1 ; col <= tableSize ; col ++ ){
+                int product = col * row;
+                result.append(String.format("%3d |", product));
+             }
+            result.append("\n");
+        }
+        String newString = result.toString();
+        System.out.println(newString);
+
+        return newString;
     }
 }
