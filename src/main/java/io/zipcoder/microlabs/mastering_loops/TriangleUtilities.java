aa8d31d288042a9fde589746f2f4961e3a4740ff
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-07-04 15:55:03 -0400

    triang test again

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,30 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        int idx = 1 ;
+        while (idx < numberOfRows) {
+            String thisRow = getRow(idx) ;
+            result.append(thisRow).append("\n");
+            idx++;
+        }
+        return result.toString();
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0 ; i < numberOfStars ; i++ ) {
+            result.append("*");
+        }
+        return result.toString();
     }
 
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(5);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(10);
     }
 }
